38e01b8c30258e123816ed656a83b6fe042d655f
david <davidteri91@gmail.com>
2014-10-31 00:37:35 +0100

    Revert "Tablet style settings [2/2]"
    This reverts commit a1d7c4f76aca7f38773056bbac2f6aa3fb47dcd9.

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -99,9 +99,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_LISTVIEW_INTERPOLATOR = "listview_interpolator";
     private static final String KEY_TOAST_ANIMATION = "toast_animation";
 
-    // Dual Panel
-    private static final String KEY_DUAL_PANEL = "force_dualpanel";
-
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
     private static final String KEY_EXPANDED_DESKTOP = "expanded_desktop";
@@ -131,7 +128,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mSunlightEnhancement;
     private CheckBoxPreference mColorEnhancement;
     private CheckBoxPreference mTapToWake;
-    private CheckBoxPreference mDualPanel;
 
     private ListPreference mListViewAnimation;
     private ListPreference mListViewInterpolator;
@@ -225,10 +221,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         disableUnusableTimeouts(mScreenTimeoutPreference);
         updateTimeoutPreferenceDescription(currentTimeout);
         updateDisplayRotationPreferenceDescription();
-
-        mDualPanel = (CheckBoxPreference) findPreference(KEY_DUAL_PANEL);
-        mDualPanel.setChecked(Settings.System.getBoolean(getContentResolver(),
-                Settings.System.FORCE_DUAL_PANEL, false));
 
         mFontSizePref = (FontDialogPreference) findPreference(KEY_FONT_SIZE);
         mFontSizePref.setOnPreferenceChangeListener(this);
@@ -644,10 +636,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     Settings.Global.WAKE_WHEN_PLUGGED_OR_UNPLUGGED,
                     mWakeWhenPluggedOrUnplugged.isChecked() ? 1 : 0);
             return true;
-        } else if (preference == mDualPanel) {
-	    Settings.System.putBoolean(getContentResolver(), Settings.System.FORCE_DUAL_PANEL,
-                    mDualPanel.isChecked() ? true : false);
-	    return true;
         } else if (preference == mTapToWake) {
             return TapToWake.setEnabled(mTapToWake.isChecked());
         }
